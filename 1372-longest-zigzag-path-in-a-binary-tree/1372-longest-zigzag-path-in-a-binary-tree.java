9e7a01a9596920a24b391df71200bc6f17f93e1e
Vatsal Raj <143717741+VatsalRaj481@users.noreply.github.com>
2026-07-11 18:12:39 +0530

    Time: 5 ms (58.41%), Space: 57.7 MB (55.95%) - LeetHub

--- 1372-longest-zigzag-path-in-a-binary-tree/1372-longest-zigzag-path-in-a-binary-tree.java
+++ 1372-longest-zigzag-path-in-a-binary-tree/1372-longest-zigzag-path-in-a-binary-tree.java
@@ -1,70 +1,21 @@
-// /**
-//  * Definition for a binary tree node.
-//  * public class TreeNode {
-//  *     int val;
-//  *     TreeNode left;
-//  *     TreeNode right;
-//  *     TreeNode() {}
-//  *     TreeNode(int val) { this.val = val; }
-//  *     TreeNode(int val, TreeNode left, TreeNode right) {
-//  *         this.val = val;
-//  *         this.left = left;
-//  *         this.right = right;
-//  *     }
-//  * }
-//  */
-// class Solution {
-//     long maxNodes=0;
-//     public int longestZigZag(TreeNode root) {
-//         if(root==null)return 0;
-//         dfs(root,'l',1);
-//         dfs(root,'r',1);
-//         longestZigZag(root.left);
-//         longestZigZag(root.right);
-//         return maxNodes-1;
-//     }
-//     private void dfs(TreeNode node,char dir,long nodes){
-//         if(node==null)return;
-//         maxNodes=Math.max(nodes,maxNodes);
-//         if(dir=='l'){
-//             dfs(node.left,'r',nodes+1);
-//         }
-//         else{
-//             dfs(node.right,'l',nodes+1);
-//         }
-//     }
-// }
-
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
 class Solution {
-    int maxNodes=0;
+    int longest = 0;
+
     public int longestZigZag(TreeNode root) {
-        dfs(root);
-        return maxNodes;
-    }   
-    private int[] dfs(TreeNode node){
-        if(node==null)return new int[]{-1,-1};
-        int leftChild[] = dfs(node.left);
-        int rightChild[] = dfs(node.right);
+        dfs(root, 0, 'l');
+        return longest;
+    }
 
-        int left = 1+leftChild[1];
-        int right=1+rightChild[0];
+    private void dfs(TreeNode root, int count, char dir) {
+        if (root == null) return;
+        longest = Math.max(longest, count);
 
-        maxNodes=Math.max(maxNodes,Math.max(left,right));
-        return new int[]{left,right};
+        if (dir == 'r') {
+            dfs(root.left, count + 1, 'l');
+            dfs(root.right, 1, 'r');
+        } else {
+            dfs(root.left, 1, 'l');
+            dfs(root.right, count + 1, 'r');
+        }
     }
 }
