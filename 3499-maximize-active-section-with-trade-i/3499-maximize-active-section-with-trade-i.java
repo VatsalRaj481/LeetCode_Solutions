2285eeb382c0e81bb3617cea0513a9e92150e58a
Vatsal Raj <143717741+VatsalRaj481@users.noreply.github.com>
2026-07-21 13:32:02 +0530

    Time: 56 ms (80.28%), Space: 47.8 MB (83.1%) - LeetHub

--- 3499-maximize-active-section-with-trade-i/3499-maximize-active-section-with-trade-i.java
+++ 3499-maximize-active-section-with-trade-i/3499-maximize-active-section-with-trade-i.java
@@ -1,31 +1,62 @@
+// class Solution {
+//     public int maxActiveSectionsAfterTrade(String s) {
+//         int originalOnes=0;
+//         for(char c:s.toCharArray()){
+//             if(c=='1')originalOnes++;
+//         }
+//         String t='1'+s+'1';
+//         List<Character> type = new ArrayList<>();
+//         List<Integer> len = new ArrayList<>();
+//         int i=0;
+//         while(i<t.length()){
+//             char ch = t.charAt(i);
+//             int j=i;
+//             while(j<t.length() && t.charAt(j)==ch){
+//                 j++;
+//             }
+//             type.add(ch);
+//             len.add(j-i);
+//             i=j;
+//         }
+
+//         int maxGain = 0;
+//         for(int k=1;k<type.size()-1;k++){
+//             if(type.get(k-1)=='0' && type.get(k)=='1' && type.get(k+1)=='0'){
+//                 maxGain = Math.max(maxGain,len.get(k-1)+len.get(k+1));
+//             }
+//         }
+
+//         return originalOnes+maxGain;
+//     }
+// }
+
 class Solution {
     public int maxActiveSectionsAfterTrade(String s) {
-        int originalOnes=0;
-        for(char c:s.toCharArray()){
-            if(c=='1')originalOnes++;
-        }
-        String t='1'+s+'1';
-        List<Character> type = new ArrayList<>();
-        List<Integer> len = new ArrayList<>();
-        int i=0;
-        while(i<t.length()){
-            char ch = t.charAt(i);
-            int j=i;
-            while(j<t.length() && t.charAt(j)==ch){
-                j++;
+        int totalOnes=0;
+        int noOfzerosBeforeOne=0;
+        int maxTransform=0;
+        for(int i=0;i<s.length();i++){
+            int noOfOnesBetweenZeros=0;
+            while(i<s.length() && s.charAt(i)=='1'){
+                noOfOnesBetweenZeros++;
+                totalOnes++;
+                i++;
             }
-            type.add(ch);
-            len.add(j-i);
-            i=j;
-        }
 
-        int maxGain = 0;
-        for(int k=1;k<type.size()-1;k++){
-            if(type.get(k-1)=='0' && type.get(k)=='1' && type.get(k+1)=='0'){
-                maxGain = Math.max(maxGain,len.get(k-1)+len.get(k+1));
+            int noOfZerosAfterOne=0;
+            while(i<s.length() && s.charAt(i)=='0'){
+                noOfZerosAfterOne++;
+                i++;
             }
-        }
 
-        return originalOnes+maxGain;
+            if(noOfzerosBeforeOne>0 && noOfOnesBetweenZeros>0 && noOfZerosAfterOne>0){
+                maxTransform=Math.max(maxTransform,
+                noOfzerosBeforeOne+noOfZerosAfterOne);
+            }
+            noOfzerosBeforeOne=noOfZerosAfterOne;
+            i--;
+
+        }
+        return totalOnes+maxTransform;
     }
 }
