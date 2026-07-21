bedef29533ea93c83cf77e13e21f5520cb537286
Vatsal Raj <143717741+VatsalRaj481@users.noreply.github.com>
2026-07-21 13:37:56 +0530

    Time: 4 ms (38.49%), Space: 44.3 MB (45.02%) - LeetHub

---
+++ 3411-maximum-subarray-with-equal-products/3411-maximum-subarray-with-equal-products.java
@@ -0,0 +1,46 @@
+class Solution {
+
+    public int maxLength(int[] nums) {
+
+        int n = nums.length;
+        int ans = 1;
+
+        for (int i = 0; i < n; i++) {
+
+            long product = 1;
+            long gcd = 0;
+            long lcm = 1;
+
+            for (int j = i; j < n; j++) {
+
+                product *= nums[j];
+
+                if (gcd == 0)
+                    gcd = nums[j];
+                else
+                    gcd = gcd(gcd, nums[j]);
+
+                lcm = lcm(lcm, nums[j]);
+
+                if (product == gcd * lcm) {
+                    ans = Math.max(ans, j - i + 1);
+                }
+            }
+        }
+
+        return ans;
+    }
+
+    private long gcd(long a, long b) {
+        while (b != 0) {
+            long t = a % b;
+            a = b;
+            b = t;
+        }
+        return a;
+    }
+
+    private long lcm(long a, long b) {
+        return a / gcd(a, b) * b;
+    }
+}
