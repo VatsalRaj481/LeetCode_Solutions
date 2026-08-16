069c88c6839749952c11a194f3947720887b83b2
Vatsal Raj <143717741+VatsalRaj481@users.noreply.github.com>
2026-08-16 16:39:50 +0530

    Time: 4 ms (75.68%), Space: 114.7 MB (64.86%) - LeetHub

--- 2029-stone-game-ix/2029-stone-game-ix.java
+++ 2029-stone-game-ix/2029-stone-game-ix.java
@@ -1,32 +1,9 @@
 class Solution {
-    public boolean stoneGameIX(int[] stones) {
-        int cnt0 = 0;
-        int cnt1 = 0;
-        int cnt2 = 0;
-
-        for (int stone : stones) {
-            int r = stone % 3;
-
-            if (r == 0) {
-                cnt0++;
-            } else if (r == 1) {
-                cnt1++;
-            } else {
-                cnt2++;
-            }
-        }
-
-        // If one of cnt1/cnt2 is zero, Alice has only one
-        // possible non-zero remainder to start with.
-        if (cnt1 == 0 || cnt2 == 0) {
-            return Math.max(cnt1, cnt2) > 2 && cnt0 % 2 == 1;
-        }
-
-        // Both remainder types exist.
-        if (cnt0 % 2 == 0) {
-            return true;
-        }
-
-        return Math.abs(cnt1 - cnt2) > 2;
-    }
+  public boolean stoneGameIX(int[] stones) {
+    int[] count = new int[3];
+    for (final int stone : stones)
+      ++count[stone % 3];
+    if (count[0] % 2 == 0) return Math.min(count[1], count[2]) > 0;
+    return Math.abs(count[1] - count[2]) > 2;
+  }
 }
