48bc400887592e0b801aafec8b7cb08bcc8deb3a
Vatsal Raj <143717741+VatsalRaj481@users.noreply.github.com>
2026-08-25 14:10:25 +0530

    Time: 0 ms (100%), Space: 42.7 MB (12.59%) - LeetHub

--- 3492-maximum-containers-on-a-ship/3492-maximum-containers-on-a-ship.java
+++ 3492-maximum-containers-on-a-ship/3492-maximum-containers-on-a-ship.java
@@ -1,12 +1,17 @@
+// class Solution {
+//     public int maxContainers(int n, int w, int maxWeight) {
+//         int containers=0,totalWeight=0;
+//         for(int i=0;i<n*n;i++){
+//             totalWeight+=w;
+//             if(totalWeight<=maxWeight){
+//                 containers++;
+//             }
+//         }
+//         return containers;
+//     }
+// }
 class Solution {
     public int maxContainers(int n, int w, int maxWeight) {
-        int containers=0,totalWeight=0;
-        for(int i=0;i<n*n;i++){
-            totalWeight+=w;
-            if(totalWeight<=maxWeight){
-                containers++;
-            }
-        }
-        return containers;
+        return Math.min(n * n, maxWeight / w);
     }
 }
