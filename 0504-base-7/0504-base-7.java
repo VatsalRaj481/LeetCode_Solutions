a3ce3ea636e34f16cf3ed9701acbb588cb075f67
Vatsal Raj <143717741+VatsalRaj481@users.noreply.github.com>
2026-09-09 14:23:21 +0530

    Time: 1 ms (73.84%), Space: 42.9 MB (41.34%) - LeetHub

--- 0504-base-7/0504-base-7.java
+++ 0504-base-7/0504-base-7.java
@@ -1,5 +1,18 @@
 class Solution {
     public String convertToBase7(int num) {
-        return Integer.toString(num,7);
+        //return Integer.toString(num,7);
+        if(num==0)return "0";
+        boolean isNegative = num<0;
+        long n = Math.abs((long)num);
+        StringBuilder result = new StringBuilder();
+        while(n>0){
+            long remainder = n%7;
+            result.append(remainder);
+            n/=7;
+        }
+        if(isNegative){
+            result.append('-');
+        }
+        return result.reverse().toString();
     }
 }
