9bafc6683289117932253adcb919b63d07331007
Vatsal Raj <143717741+VatsalRaj481@users.noreply.github.com>
2026-09-18 13:49:41 +0530

    Time: 0 ms (100%), Space: 43 MB (51.92%) - LeetHub

--- 0709-to-lower-case/0709-to-lower-case.java
+++ 0709-to-lower-case/0709-to-lower-case.java
@@ -1,8 +1,8 @@
 class Solution {
     public String toLowerCase(String s) {
-        char[] arr = new char[s.length()];
-        for(int i=0;i<s.length();i++){
-            arr[i]=Character.toLowerCase(s.charAt(i));
+        char[] arr = s.toCharArray();
+        for(int i=0;i<arr.length;i++){
+            arr[i]=Character.toLowerCase(arr[i]);
         }
         return new String(arr);
     }
